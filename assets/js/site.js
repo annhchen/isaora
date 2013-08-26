fc37d4eea62c33023b6f5d53bbcaf0e707755539
Mindy Tchieu <mtchieu@gmail.com>
2013-08-26 14:35:07 -0400

    structure

---
+++ assets/js/site.js
@@ -0,0 +1,15 @@
+$(function() {
+    var isaora = {
+        config: {
+        },
+
+        init: function () {
+            console.log('initing');
+        },
+
+        bindEvents: function () {
+        }
+    };
+
+    $(document).ready(isaora.init());
+});
