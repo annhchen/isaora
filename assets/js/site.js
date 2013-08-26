e50916459c0d3802d72cdd623eb96b1e0106132c
Mindy Tchieu <mtchieu@gmail.com>
2013-08-26 16:11:07 -0400

    adding instafeed and tumblr posts

--- assets/js/site.js
+++ assets/js/site.js
@@ -3,7 +3,7 @@ $(function() {
         config: {
             apis: {
                 twitter: '/twitter-proxy.php?url=',
-                tumblr: '',
+                tumblr: 'http://isaora.tumblr.com/api/read/json',
                 instagram: 'https://api.instagram.com/v1/users/isaora'
             }
         },
@@ -20,6 +20,9 @@ $(function() {
 
         fetchData: function () {
             Isaora.fetchTwitterPosts();
+            Isaora.fetchTumblrPosts();
+            Isaora.fetchInstagramPosts();
+
         },
 
         fetchTwitterPosts: function () {    
@@ -27,6 +30,7 @@ $(function() {
                 encodeURIComponent('statuses/user_timeline.json?screen_name=Isaora&count=10')
                 // Extra params: 
                 // encodeURIComponent('&count=10&include_rts=false&exclude_replies=true');
+
             console.log('twitter url: ' + twitterUrl);
             $.getJSON(twitterUrl)
                 .done(function(response) {
@@ -37,7 +41,32 @@ $(function() {
                     var err = textStatus + ', ' + error;
                     console.log( "Twitter request failed: " + err);
                 });
+        },
+
+        fetchInstagramPosts: function () {
+            var instagramFeed = new Instafeed({
+                get: 'user',
+                userId: 145933309,
+                accessToken: '145933309.467ede5.044b153f6f1b45508c95c019c47a6ce2'
+            });
+            instagramFeed.run();
+        },
+
+        fetchTumblrPosts: function () {
+            console.log('tumblr api:');
+            console.log(tumblr_api_read);
+            
+            // $.getJSON(Isaora.config.apis.tumblr)
+            //     .done(function(response) {
+            //         console.log('\n success! tumblr data: ');
+            //         console.log(response);
+            //     })
+            //     .fail(function(jqxhr, textStatus, error) {
+            //         var err = textStatus + ', ' + error;
+            //         console.log( "Tumblr request failed: " + err);
+            //     });
         }
+
     };
 
     $(document).ready(Isaora.init());
